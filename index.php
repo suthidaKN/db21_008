c26aa3df3952f7c473bd7b75d5e153ac80e86eeb
suthidaKN <91516432+suthidaKN@users.noreply.github.com>
2021-09-30 02:35:12 +0700

    create index and routes

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+
+if(isset($_GET['controller'])&&isset($_GET['action']))
+{
+    $controller = $_GET['controller'];
+    $action = $_GET['action'];
+}else
+{
+    $controller = 'pages';
+    $action = 'home';
+}
+?>
+<html>
+<head></head>
+    <body>
+    <?php echo "controller = ".$controller.", action = ".$action;?>
+    <br>[<a href="?controller=pages&action=home">Home </a>] 
+        [<a href="?controller=quotation&action=index">quotation</a>]
+        [<a href="?controller=quotationDetail&action=index">quotationDetail</a>]
+    <br>
+
+   
+
+    <?php require_once("./routes.php"); ?>
+    
+</body>   
+</html>
