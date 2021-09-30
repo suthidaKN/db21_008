640c836c78807883a73107f1c8636d52b5b00f77
suthidaKN <91516432+suthidaKN@users.noreply.github.com>
2021-09-30 20:50:12 +0700

    Update index.php

--- index.php
+++ index.php
@@ -14,9 +14,11 @@ if(isset($_GET['controller'])&&isset($_GET['action']))
 <head></head>
     <body>
     <?php echo "controller = ".$controller.", action = ".$action;?>
-    <br>[<a href="?controller=pages&action=home">Home </a>] 
-        [<a href="?controller=quotation&action=index">quotation</a>]
-        [<a href="?controller=quotationDetail&action=index">quotationDetail</a>]
+    <br>[<a href="http://158.108.207.4/db21/db21_008/?controller=pages&action=home">Home </a>] 
+        [<a href="http://158.108.207.4/db21/db21_008/?controller=quotation&action=index">quotation</a>]
+        [<a href="http://158.108.207.4/db21/db21_008/?controller=quotationDetail&action=index">quotationDetail</a>]
+        [<a href="http://158.108.207.4/db21/db21_008/?controller=product&action=index">product</a>]
+      
     <br>
 
    
