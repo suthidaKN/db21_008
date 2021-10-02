2bfadf68f416be06d9e02e91892f235016f6c0e6
suthidaKN <91516432+suthidaKN@users.noreply.github.com>
2021-10-02 15:55:08 +0700

    Update index.php

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ if(isset($_GET['controller'])&&isset($_GET['action']))
     <body>
     <?php echo "controller = ".$controller.", action = ".$action;?>
     <br>[<a href="?controller=pages&action=home">Home </a>] <br>
-    <?php require_once("routes.php"); ?>
+    <r?php require_once("routes.php"); ?>
     
 </body>   
 </html>
