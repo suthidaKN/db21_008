49842a24b67cffe2159876922233f6a1e5bf660e
SUNEASTER <kiattichaiza01@gmail.com>
2021-10-03 20:41:29 +0700

    ss

--- index.php
+++ index.php
@@ -18,6 +18,9 @@ if(isset($_GET['controller'])&&isset($_GET['action']))
     [<a href="?controller=Quotation&action=index">Quotation </a>]
     [<a href="?controller=QuotationDetail&action=index"> QuotationDetail </a>]
     [<a href="?controller=ProductRate&action=index">ProductRate</a>]
+
+
+    
     [<a href="?controller=ProductRate&action=index">sunx</a>]
     <?php require_once("./routes.php");?>
     
